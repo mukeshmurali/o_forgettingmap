ae7ddfccff616fac91f99e8be47a455609a12038
Mukesh Murali <mukesh.murali@gamesys.co.uk>
2021-09-20 17:40:40 +0100

    Adding Map class with logic

--- src/main/java/com/test/forgettingmap/ForgettingMap.java
+++ src/main/java/com/test/forgettingmap/ForgettingMap.java
@@ -1,21 +1,54 @@
 package com.test.forgettingmap;
 
-import java.util.HashMap;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ForgettingMap<K,V> {
     private HashMap<K, V> associationMap;
+    private TreeMap<K, Integer> accessKeyTracker;
+
     private int size;
 
     public ForgettingMap(int size) {
         this.size = size;
         this.associationMap=new HashMap<K,V>();
+        this.accessKeyTracker=new TreeMap<>();
     }
 
     public void add(K key, V value) {
-        associationMap.put(key,value);
+        if (associationMap.size() < size) {
+            updateTrackerAndAdd(key, value);
+            return;
+        }
+        addAfterRemovingLeastUsed(key,value);
+    }
+
+    private void updateTrackerAndAdd(K key, V value) {
+        updateKeyTracker(key, 0);
+        associationMap.put(key, value);
+    }
+
+    private void addAfterRemovingLeastUsed(K key, V value) {
+        V keyToRemove = associationMap.remove(findLeastUsed());
+        accessKeyTracker.remove(keyToRemove);
+        updateTrackerAndAdd(key, value);
+    }
+
+    private K findLeastUsed() {
+        return accessKeyTracker.entrySet()
+                .stream()
+                .sorted(Comparator.comparing(Map.Entry::getValue))
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList()).get(0);
     }
 
     public V find(K key) {
+        updateKeyTracker(key, 1);
         return associationMap.get(key);
+    }
+
+    private void updateKeyTracker(K key, int value) {
+        Integer callCount = Optional.ofNullable(accessKeyTracker.get(key)).orElse(0);
+        callCount = callCount==0 ? accessKeyTracker.put(key, value):accessKeyTracker.put(key, callCount + 1);
     }
 }
--- src/test/java/com/test/forgettingmap/ForgettingMapTest.java
+++ src/test/java/com/test/forgettingmap/ForgettingMapTest.java
@@ -16,4 +16,62 @@ public class ForgettingMapTest {
         //assert
         assertThat(fmap.find(key)).isEqualTo(value);
     }
+
+    @Test
+    void check_Addingmorethanmaxsize_replacesOldest_checkIfOldIsDeleted() {
+        //arrange
+        int size=2;
+        ForgettingMap fmap=new ForgettingMap(size);
+        //act
+        fmap.add(1, 10);
+        fmap.add(2, 20);
+
+        //Accessing key 1 multiple times
+        fmap.find(1);
+        fmap.find(1);
+        fmap.add(3, 30);
+        //assert
+        assertThat(fmap.find(2)).isNull();
+    }
+
+    @Test
+    void check_Addingmorethanmaxsize_replacesOldest_checkIfNewIsAdded() {
+        //arrange
+        int size=2;
+        ForgettingMap fmap=new ForgettingMap(size);
+        //act
+        fmap.add(1, 10);
+        fmap.add(2, 20);
+
+        //Accessing key 1 multiple times
+        fmap.find(1);
+        fmap.find(1);
+        fmap.find(2);
+        fmap.find(2);
+        fmap.find(2);
+        fmap.add(3, 30);
+        //assert
+        assertThat(fmap.find(3)).isEqualTo(30);
+    }
+
+    @Test
+    void check_Addingmorethanmaxsize_replacesOldest_checkIfOldIsRemoved_EqualAccessCount() {
+        //arrange
+        int size=2;
+        ForgettingMap fmap=new ForgettingMap(size);
+        //act
+        fmap.add(1, 10);
+        fmap.add(2, 20);
+
+        //Accessing key 1 multiple times
+        fmap.find(1);
+        fmap.find(2);
+        fmap.find(2);
+        fmap.find(1);
+        fmap.add(8, 80);
+        //assert
+        assertThat(fmap.find(1)).isNull();
+        assertThat(fmap.find(2)).isEqualTo(20);
+    }
+
 }
